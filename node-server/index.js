6ea63498330ec0eec28198bf0593466d8ec3c2bd
steven85048 <steven85048@gmail.com>
2018-10-10 23:46:37 -0700

    Add node server and angular app

---
+++ node-server/index.js
@@ -0,0 +1,7 @@
+var github = require('octonode');
+
+var client = github.client();
+
+client.get('/users/pksunkara', {}, function (err, status, body, headers) {
+  console.log(body); //json object
+});
